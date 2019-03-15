1744ed69e6800bbf882541c5fb89c5074378acdb
Brian Mwevi <mwevibrian@gmail.com>
2019-03-15 22:16:30 +0300

    additional files and changes for Heroku

--- writers/settings/production.py
+++ writers/settings/production.py
@@ -118,7 +118,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
