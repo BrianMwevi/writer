0b4ca4f979d07d50c8f6cd522d265f90965cf0b4
Brian Mwevi <mwevibrian@gmail.com>
2019-03-19 09:48:51 +0300

    Added form_class and cdn link for fonts-awesome

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,6 @@ app_name = 'accounts'
 
 urlpatterns = [
 	path('register/', SignUpView.as_view(), name='register'),
-	path('login/', auth_views.LoginView.as_view(template_name='accounts/register.html', success_url=reverse_lazy('writersapp:home')), name='login'),
+	path('login/', auth_views.LoginView.as_view(template_name='accounts/register.html',form_class=LoginForm, success_url=reverse_lazy('writersapp:home')), name='login'),
 	path('logout/', auth_views.LogoutView.as_view(), name='logout', kwargs={'next_page':'writersapp:home'}),
 ]
