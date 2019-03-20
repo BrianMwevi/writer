fbbc950dd1a0812593696148a6b1c97db16c77ad
Brian Mwevi <mwevibrian@gmail.com>
2019-03-20 21:04:36 +0300

    Commented forms.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import CreateView
-from .forms import SignupForm, LoginForm
+# from .forms import SignupForm, LoginForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
 from django.urls import reverse_lazy
 # Create your views here.
 class SignUpView(CreateView):
-	form_class = SignupForm
+	form_class = UserCreationForm
 	model = User
 	success_url = reverse_lazy('accounts:register')
 	template_name = 'accounts/register.html'
 	def get_context_data(self, **kwargs):
 	    context = super().get_context_data(**kwargs)
-	    context['login_form'] =  LoginForm
+	    # context['login_form'] =  LoginForm
 	    # context['action_url'] =  LoginForm
 	    return context
