3e0c8003557ddd681f8e3f07d87b48a4b8bad0de
Brian Mwevi <mwevibrian@gmail.com>
2019-04-17 15:51:09 +0300

    User can request to change his/her passwd

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, re_path
 from django.urls import reverse_lazy
-from .views import register
+from .views import register, password_change
 from django.contrib.auth import views as auth_views
 from .forms import LoginForm
 
@@ -9,25 +9,5 @@ app_name = 'accounts'
 urlpatterns = [
 	path('register/', register, name='register'),
 	path('logout/', auth_views.LogoutView.as_view(), name='logout', kwargs={'next_page':'writersapp:home'}),
-	path('password/change/', 
-	        auth_views.PasswordChangeView.as_view(template_name='accounts/pass_reset.html'), 
-	        name='password_change'),
-	path('password/change/done/',
-	        auth_views.PasswordChangeDoneView.as_view(), 
-	        name='password_change_done'),
-	path('password/reset/', 
-	        auth_views.PasswordResetView.as_view(), 
-	        name='password_reset'),
-	path('password/reset/done/', 
-	        auth_views.PasswordResetDoneView.as_view(), 
-	        name='password_reset_done'),
-	re_path('password/reset/\
-	        (?P<uidb64>[0-9A-Za-z_\-]+)/\
-	        (?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/', 
-	        auth_views.PasswordResetConfirmView.as_view(template_name='writersapp:home'), 
-	        name='password_reset_confirm'),
-
-	path('password/reset/complete/', 
-	        auth_views.PasswordResetCompleteView.as_view(), 
-        name='password_reset_complete'),
+	path('password/', password_change, name="password_change"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 from .forms import SignupForm, LoginForm
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
 from django.http import HttpResponse, HttpResponseRedirect
 
-from django.contrib.auth import get_user_model, authenticate, login, logout
+from django.contrib.auth import get_user_model, authenticate, login, logout, update_session_auth_hash
+from django.contrib import messages
 
 
 from django.urls import reverse_lazy
@@ -41,3 +42,19 @@ def register(request):
 			if form.errors:
 				login_form = form
 	return render(request, 'accounts/register.html', {"login_form":login_form, "signup_form":signup_form, "is_user":is_user})
+
+def password_change(request):
+	if request.method == 'POST':
+		form = PasswordChangeForm(request.user, request.POST)
+		if form.is_valid():
+			user = form.save()
+			update_session_auth_hash(request,user)
+			messages.success(request, 'Your password was successfully updated!')
+			return redirect('home')
+		else:
+			messages.error(request, 'Please correct the error below.')
+	else:
+		form = PasswordChangeForm(request.user)
+	return render(request, 'accounts/pass_reset.html', {
+	'form': form
+		})
